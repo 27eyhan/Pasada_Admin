1a6b1a404250349e8022adb6d90545e1ef3ebe0d
Ethan Humarang <humarangethan@gmail.com>
2025-04-10 09:21:19 +0800

    change the name of mapScreen to map_screen

--- pasada_admin_application/lib/maps/map_screen.dart
+++ pasada_admin_application/lib/maps/map_screen.dart
@@ -1,7 +1,7 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 
 class Mapscreen extends StatefulWidget {
   const Mapscreen({super.key});
