e63aee0daea7ba65aba7d6eaa2d615dfd84bbae8
Ethan Humarang <humarangethan@gmail.com>
2025-04-28 20:29:57 +0800

    map_screen insert but not working

--- pasada_admin_application/lib/screen/main_pages/dashboard_pages/dashboard.dart
+++ pasada_admin_application/lib/screen/main_pages/dashboard_pages/dashboard.dart
@@ -2,22 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:pasada_admin_application/config/palette.dart';
 import 'package:pasada_admin_application/screen/appbars_&_drawer/appbar_search.dart';
 import 'package:pasada_admin_application/screen/appbars_&_drawer/drawer.dart';
+import 'package:pasada_admin_application/maps/map_screen.dart';
 
 class Dashboard extends StatefulWidget {
   @override
   _DashboardState createState() => _DashboardState();
 }
 
-  class _DashboardState extends State<Dashboard> {
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       backgroundColor: Palette.whiteColor,
       appBar: AppBarSearch(),
       drawer: MyDrawer(),
-      body: Center(
-        child: Text('Dashboard Page'),
-      ),
+      body: Mapscreen(),
     );
   }
 }
